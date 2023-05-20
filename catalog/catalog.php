fb35af2666730e74494f4a8965ff54de67949208
ramarefin <rama.223040040@mail.unpas.ac.id>
2023-05-20 16:16:56 +0700

    progress penambahan 1

---
+++ catalog/catalog.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catalog</title>
+    <link rel="stylesheet" href="catalog.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+
+<body>
+    <header>
+        <a href="#" class="logo">
+            <img src="img/icon.png" alt="icon">
+        </a>
+        <!-- menu icon -->
+        <i class='bx bx-menu' id="menu-icon"></i>
+        <!-- links -->
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About us</a></li>
+            <li><a href="#product">Product</a></li>
+            <li><a href="#customer">customer</a></li>
+        </ul>
+        <!-- icon -->
+        <div class="header-icon">
+            <i class='bx bx-cart'></i>
+            <i class='bx bx-search' id="search-icon"></i>
+        </div>
+        <!-- search box -->
+        <div class="search-box">
+            <input type="search" name="" id="" placeholder="Search here...">
+        </div>
+    </header>
+
+</body>
+
+</html>
